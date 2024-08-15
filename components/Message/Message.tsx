3e9ffdaa5b5086ffd9ca8938de70e69fcbf2f820
DamperDoor66 <ludmit@outlook.com>
2024-08-15 15:46:18 -0300

    fix: :bug: add spanish words and fix issue

--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -1,4 +1,4 @@
-import { Message } from "@/interfaces/Message/Message";
+import type { Message } from "@/interfaces/Message/Message";
 
 export function Message({ isWinner, isLoser,wordToGuess}: Message) {
    return (
@@ -8,11 +8,12 @@ export function Message({ isWinner, isLoser,wordToGuess}: Message) {
         rounded-xl text-[#363c3a] z-50 p-20 flex flex-col gap-10
         border-[#006d77] border-2 top-3">
           <div>
-          {isWinner && "You won! You're great :D"}
-          {isLoser && `Nice Try, the word was '${wordToGuess}'`}
+          {isWinner && "Ganaste! Que genial sos! :D <3"}
+          {isLoser && `Buen intento! uwu 
+           Pero la palabra es: '${wordToGuess}'`}
           </div>
           <div className="bg-[#006d77] rounded-xl p-5 text-white">
-          <a href="/">Want to try again?</a>
+          <a href="/">Queres intentar de nuevo?</a>
           </div>
         </div> : null
       }
